6cbccbdc6a9a3545cf9055fe554037057c7fa46b
santt99 <santiago10.jbch@gmail.com>
2019-08-09 20:36:36 -0500

    Delete UI added

--- src/components/notes/Note.js
+++ src/components/notes/Note.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-function Note({ title, note }) {
+function Note({ title, note, key }) {
 	return (
-		<div className="card indigo lighten-2">
-			<div className="card-content white-text">
-				<span className="card-title">{title}</span>
-				<p>{note}</p>
+		<div className="col s6">
+			<div className="card indigo lighten-2">
+				<div className="card-content white-text">
+					<div className="row">
+						<div className="col s10">
+							<span className="card-title">{title}</span>
+							<p>{note}</p>
+						</div>
+						<div className="col s2">
+							<a class="btn-floating btn-large waves-effect waves-light red">
+								<i class="material-icons">delete</i>
+							</a>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
--- src/components/notes/index.js
+++ src/components/notes/index.js
@@ -3,13 +3,17 @@ import Note from './Note';
 import { connect } from 'react-redux';
 
 function NotesList({ notes }) {
-	return notes.map(({ title, note }, index) => {
+	if (notes.length > 0) {
 		return (
-			<div className="col s4" key={index}>
-				<Note title={title} note={note} />
+			<div className="row">
+				{notes.map(({ title, note }, index) => {
+					return <Note title={title} note={note} key={index} />;
+				})}
 			</div>
 		);
-	});
+	} else {
+		return <h1>You don't have notes, please go to the "new note" screen and create one.</h1>;
+	}
 }
 
 const mapStateToProps = (state) => {
